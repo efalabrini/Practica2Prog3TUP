8374023a85669ba453d3f1129a44bf52aa022e31
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-07 20:35:22 -0300

    Create PhotoBook.cs

---
+++ Web/Ej2/PhotoBook.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Ej2
+{
+    public class PhotoBook
+    {
+        private int numPages {  get; set; }
+
+        public PhotoBook(int cantPag = 16) 
+        {
+            numPages = cantPag;
+        }
+
+        public int GetNumberPages() 
+        { 
+            return numPages; 
+        }
+    }
+}
