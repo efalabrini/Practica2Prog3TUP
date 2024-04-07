d4571568f38fd02a00e3fb74a7ce7e03859e384a
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-07 20:35:35 -0300

    Create Ej2Controller.cs

---
+++ Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej2;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get()
+        {
+            var book1 = new PhotoBook();
+            var book2 = new PhotoBook(24);
+            var book3 = new BigPhotoBook();
+
+            return $"El primer libro tiene: {book1.GetNumberPages()}\nEl segundo libro tiene: {book2.GetNumberPages()}\nEl tercer libro tiene: {book3.GetNumberPages()}";
+        }
+    }
+}
