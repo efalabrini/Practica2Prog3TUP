290b93036b353cd549c0a4dff2057127eb395484
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-07 20:41:14 -0300

    Texto explicativo para entender el constructor por herencia

--- Web/Ej2/BigPhotoBook.cs
+++ Web/Ej2/BigPhotoBook.cs
@@ -2,7 +2,8 @@
 {
     public class BigPhotoBook : PhotoBook
     {
-        public BigPhotoBook(int cantPag = 64) : base(cantPag)
+        public BigPhotoBook(int cantPag = 64) : base(cantPag) //le pasa por parametro la cantPag definida en este constructor
+                                                              // para que haga lo mismi que en la clase base solo que con otro valor
         {
             
         }
