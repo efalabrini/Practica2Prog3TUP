b2b5b4e1d05efa721a2a0bcd7c8e21e0ba7b3d9d
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-10 00:08:42 -0300

    Create Student.cs

---
+++ Web/Ej3/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace Web.Ej3
+{
+    public class Student : Person
+    {
+        public Student(string nombre) : base(nombre)
+        {
+
+        }
+
+        public string Study()
+        {
+            return "Estoy estudiando";
+        }
+
+        public string SeeAge()
+        {
+            return $"Mi edad es: {Age} años";
+        }
+
+        public new string GetSaludo() //new para pisar el metodo de la clase Person
+        {
+            return $"Hola soy el estudiante {Name}";
+        }
+    }
+}
