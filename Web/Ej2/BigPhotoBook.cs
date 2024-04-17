79f4cf1185be84a863108891ce4cf96c695ec251
facundolgomez <facundolgomez87@gmail.com>
2024-04-17 15:08:01 -0300

    resolviendo ej2

---
+++ Web/Ej2/BigPhotoBook.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Ej2
+{
+    public class BigPhotoBook: PhotoBook
+    {
+
+        public BigPhotoBook() : base(64) //base llama al constructor de la clase base (PhotoBook)
+        {
+            
+        }
+            
+    }
+}
+
+
