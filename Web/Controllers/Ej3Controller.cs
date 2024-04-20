0a00263bc74acadf52eacdceaee01f9be82b938f
facundolgomez <facundolgomez87@gmail.com>
2024-04-20 17:43:22 -0300

    resolviendo ej3

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Ej3;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ActionResult<string> GetEstudiante([FromQuery] string nombre, [FromQuery] int edad)
+        {
+            Estudiante estudiante = new Estudiante();
+            estudiante.SetEdad(edad);
+            return Ok($"{estudiante.Saludar(nombre)}. {estudiante.MostrarEdad(estudiante.GetEdad())}");
+        }
+        
+        
+        [HttpGet("[action]")]
+        public ActionResult<string> GetProfesor([FromQuery] string nombre, [FromQuery] int edad)
+        {
+            Profesor profesor = new Profesor();
+            profesor.SetEdad(edad);
+            return Ok($"{profesor.Saludar(nombre)}. Mi edad es {profesor.GetEdad()}. {profesor.Explicar()}");
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Web/Ej3/Profesor.cs
@@ -0,0 +1,23 @@
+﻿using Web.Ej3;
+
+namespace Web.Ej3
+{
+    public class Profesor : Persona
+    {
+        
+        public string Explicar ()
+        {
+            return "Estoy explicando";
+        }
+
+        public override string Saludar(string nombre)
+        {
+            return $"Hola soy el profesor {nombre}";
+        }
+
+        public int GetEdad()
+        {
+            return Edad;
+        }
+    }
+}
