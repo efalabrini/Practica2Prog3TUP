e8cc41ef0cbb41e5a7bf34ccb05d28ecc0de979b
facundolgomez <facundolgomez87@gmail.com>
2024-04-23 03:24:48 -0300

    resolviendo ej5

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Web.Ej5;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ActionResult GetPruebaAuto([FromQuery] int cantidadCombustible)
+        {
+            Coche coche = new Coche(0);
+            coche.CargarCombustible(cantidadCombustible);
+            return Ok(coche.Conducir());
+        }
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Web/Ej5/Coche.cs
@@ -0,0 +1,26 @@
+﻿namespace Web.Ej5
+{
+    public class Coche : IVehiculo
+    {
+        private int gasolina;
+        public Coche(int cantidadGasolina) {
+            gasolina = cantidadGasolina;
+        }
+
+        public string Conducir()
+        {
+            if (gasolina > 0)
+            {
+                return "El coche esta conduciendo";
+            }
+            return "No hay gasolina";
+
+        }
+
+        public bool CargarCombustible(int cantidadCombustible)
+        {
+            gasolina += cantidadCombustible;
+            return true;
+        }
+    }
+}
