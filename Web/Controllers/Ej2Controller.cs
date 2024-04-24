a7fd86dc1cd1915b9ac113f44b7396bff500fdc0
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 15:17:26 -0300

    Update Ej2Controller.cs

--- Web/Controllers/Ej2Controller.cs
+++ Web/Controllers/Ej2Controller.cs
@@ -37,23 +37,6 @@ namespace Web.Controllers
         [HttpGet("[action]")]
         public string GetNumPages([FromQuery] int id) 
         {
-            /*var books = new List<PhotoBook>();
-
-            var book3 = new BigPhotoBook();
-            books.Add(book3);
-
-            var book4 = new PhotoBook();
-            books.Add(book4);
-
-            var book5 = new PhotoBook(24);     esta fue la primer idea pero se me pisaban los id cada vez que consultaba uno nuevo
-                                               ya que se generaban nuevos objt sobre las mismas var y era una lista diferente a la hora de una segunda busqueda
-            books.Add(book5);
-
-            var book6 = new BigPhotoBook(96);
-            books.Add(book6);
-
-            var book7 = new PhotoBook(17);
-            books.Add(book7);*/
 
             PhotoBook.genId = 1;  // reinicie el id ya que si primero hacia consultas
                                   // en los otros endpoints el id aumentaba y yo no sabia con que id se generaban los libros de la lista
