e40619b35a84a5ae12370b1d383c6503b4a670e3
alanmoex <alan.moex@gmail.com>
2024-04-24 13:26:21 -0300

    Create PhotoBook.cs

---
+++ Web/Ej2/PhotoBook.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.Ej2
+{
+    public class PhotoBook
+    {   
+        public PhotoBook() 
+        {
+            NumPages = 16;
+        }
+
+        public PhotoBook(int numPages)
+        {
+            NumPages=numPages;
+        }
+        protected int NumPages { get; set; }
+
+        public int GetNumberPages()
+        {
+            return NumPages;
+        }
+    }
+}
