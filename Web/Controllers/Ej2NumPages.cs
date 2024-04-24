1432dffdc7d8b865352f4b8a958a0e0f2855f43a
alanmoex <alan.moex@gmail.com>
2024-04-24 13:26:32 -0300

    Create Ej2NumPages.cs

---
+++ Web/Controllers/Ej2NumPages.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej2;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class Ej2NumPagesController : ControllerBase
+    {
+
+        [HttpGet()]
+        public int Get([FromQuery] int index)
+        {
+            var photoBooksList = PhotoBooksListInstance.List;
+
+            return photoBooksList.GetPhotoBookPages(index);
+        }
+    }
+}
