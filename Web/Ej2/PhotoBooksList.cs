fd102949770a7b2728ef43c1de74adeed302002a
alanmoex <alan.moex@gmail.com>
2024-04-24 13:26:12 -0300

    Create PhotoBooksList.cs

---
+++ Web/Ej2/PhotoBooksList.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Ej2
+{
+    public class PhotoBooksList
+    {
+        public List<PhotoBook> photoBooksList { get; set; } = new List<PhotoBook>();
+
+        public void AddPhotoBook(PhotoBook photoBook)
+        {
+            photoBooksList.Add(photoBook);
+        } 
+
+        public int CountPhotoBooks()
+        {
+            return photoBooksList.Count();
+        }
+
+        public int GetPhotoBookPages(int index) 
+        {
+            return photoBooksList[index].GetNumberPages();
+        }
+    }
+
+
+}
