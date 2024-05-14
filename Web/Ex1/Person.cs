f9b45c7a6978aac2d20246734efbff4218598952
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-05-14 10:14:00 -0300

    ex1 done

---
+++ Web/Ex1/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Ex1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name) {
+            Name = name;
+        }
+
+        public string GetGreet()
+        {
+            return $"Hola mi nombre es {Name}";
+        }
+    }
+}
